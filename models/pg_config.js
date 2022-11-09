e792a115547f0f341c9b2369fac1ce823e8217ac
Viết Tài Tống <taitong.25052002@gmail.com>
2022-11-09 13:44:25 +0700

    Added my project

--- models/pg_config.js
+++ models/pg_config.js
@@ -1,13 +1,13 @@
 const Pool= require('pg').Pool;
 const pg_conn = new Pool({
-    user: 'uyuzvqscgfeuse',
-    host: 'ec2-44-205-177-160.compute-1.amazonaws.com',
-    database: 'dcn97s3142d4p8',
-    password: '3ea3f3112c7dcc1b583aecb690ad439dc34f9bae440c110987526de4da0dca79',
+    user: 'fzicwkdmwdfzsn',
+    host: 'ec2-44-195-132-31.compute-1.amazonaws.com',
+    database: 'd45iqkm8pu7ef6',
+    password: 'ee989e72f40d3519122e7f9c3a3ad09f00ace5b9dfb175e1a60f11353950e162',
     port: 5432,
     ssl:{
             rejectUnauthorized:false,
     },
   })
 
-module.exports= pg_conn;
+module.exports= pg_conn;    
